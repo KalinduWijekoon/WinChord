607922f6d660d2a8f27af171268a9ca3b446028e
Kalindu Wijekoon <mihiranga1996@gmail.com>
2020-03-17 23:15:24 +0530

    .idea committing

--- music/urls.py
+++ music/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
 
     # /music/
@@ -10,4 +12,6 @@ urlpatterns = [
 
     # /music/<album no(712)>
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    # /music/<album_id>/favorite/>
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
