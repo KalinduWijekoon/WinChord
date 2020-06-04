53bf709744804f2343868724760aff31998f0159
Kalindu Wijekoon <mihiranga1996@gmail.com>
2020-06-04 19:51:08 +0530

    .idea committing

--- website/urls.py
+++ website/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 
 urlpatterns = [
@@ -22,3 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('music/', include('music.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
