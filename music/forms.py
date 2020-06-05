1501ad08682e80ed045bc3a3be73de3fa2593b10
Kalindu Wijekoon <mihiranga1996@gmail.com>
2020-06-05 14:18:01 +0530

    .idea committing

--- music/forms.py
+++ music/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.models import User
+
 from .models import Album, Song
 
 
@@ -23,3 +24,11 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+#class LoginForm(forms.ModelForm):
+#    password = forms.CharField(widget=forms.PasswordInput)
+#
+#    class Meta:
+#        model = User
+#        fields = ['username', 'password']
+
--- music/urls.py
+++ music/urls.py
@@ -8,18 +8,22 @@ urlpatterns = [
 
     # /music/
     # url(r'^admin/', admin.site.urls),
-    url(r'^$', views.IndexView.as_view(), name='index'),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login_user/$', views.login_user, name='login_user'),
+    url(r'^logout_user/$', views.logout_user, name='logout_user'),
     # /music/<album no(712)>
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<song_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^songs/(?P<filter_by>[a-zA_Z]+)/$', views.songs, name='songs'),
     # /music/album/add/
-    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
-
+    url(r'^create_album/$', views.create_album, name='create_album'),
+    url(r'^(?P<album_id>[0-9]+)/create_song/$', views.create_song, name='create_song'),
+    url(r'^(?P<album_id>[0-9]+)/delete_song/(?P<song_id>[0-9]+)/$', views.delete_song, name='delete_song'),
     # /music/album/2/
-    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
-
+    #url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
     # /music/album/2/delete
-    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+    url(r'^(?P<album_id>[0-9]+)/delete_album/$', views.delete_album, name='album-delete'),
+    url(r'^(?P<album_id>[0-9]+)/favorite_album/$', views.favorite_album, name='favorite_album'),
 
 ]
